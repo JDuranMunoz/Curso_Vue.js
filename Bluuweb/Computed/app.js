da1d97b7823d5102cbf9f55b6e309232d9a56aa2
JDuranMunoz <javiereduardodm7@gmail.com>
2022-02-22 19:42:17 -0600

    Termine la carpeta 'Computed'

--- Bluuweb/Computed/app.js
+++ Bluuweb/Computed/app.js
@@ -1,11 +1,19 @@
 const app = new Vue({
     el: "#app",
     data: {
-        mensaje: "Propiedades computadas"
+        mensaje: "Propiedades computadas",
+        contador: 0
     },
     computed: {
         invertido() {
             return this.mensaje.split("").reverse().join("");
+        },
+        color() {
+            return {
+                "bg-success": this.contador <= 10,
+                "bg-warning": this.contador >10 && this.contador <= 20,
+                "bg-danger": this.contador >= 21
+            }
         }
     }
 });
