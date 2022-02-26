dc669ab13b83c076b9726be74e3ba1085d795fe5
JDuranMunoz <javiereduardodm7@gmail.com>
2022-02-25 18:54:47 -0600

    Termine la carpeta 'Comunicacion entre componentes'

--- Bluuweb/Comunicacion_entre_componentes/Componentes/hijo.js
+++ Bluuweb/Comunicacion_entre_componentes/Componentes/hijo.js
@@ -3,11 +3,21 @@ Vue.component("hijo", {
     `
     <div class="py-5 bg-success mt-3">
         <h4>Componente hijo: {{numero}}</h4>
+        <h4>Nombre: {{nombre}}</h4>
     </div>
     `,
 
     // Los 'props' son un array con un nombre.
     // El nombre debe ser el que se coloca en: <hijo></hijo> del padre.js
     // Se usan para recibir información
-    props: ["numero"]
+    props: ["numero"],
+    data() {
+        return {
+            nombre: "Javier"
+        }
+    },
+    // Se ejecuta una vez que el DOM está listo
+    mounted() {
+        this.$emit("nombreHijo", this.nombre);
+    }
 });
