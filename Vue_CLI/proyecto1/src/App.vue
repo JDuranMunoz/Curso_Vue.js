703a8f92391fd684f3b57d60545d8a3e981b2354
JDuranMunoz <javiereduardodm7@gmail.com>
2022-03-02 21:44:49 -0600

    Inicie la carpeta 'Vue_CLI/proyecto1'

---
+++ Vue_CLI/proyecto1/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-3 text-center">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <CabeceraC></CabeceraC>
+  </div>
+</template>
+
+<script>
+import CabeceraC from './components/CabeceraC.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CabeceraC
+  }
+}
+</script>
---
+++ Vue_CLI/proyecto1/src/components/CabeceraC.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        <br>
+        <h1>Frutas</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CabeceraC'
+    }
+</script>
