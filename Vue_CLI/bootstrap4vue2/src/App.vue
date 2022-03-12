5a7fadf3f898e03711e355eb20073c68c7c7dd1a
JDuranMunoz <javiereduardodm7@gmail.com>
2022-03-11 18:30:46 -0600

    Termine la carpeta 'bootstrap4vue2-Columnas responsivas'

--- Vue_CLI/bootstrap4vue2/src/App.vue
+++ Vue_CLI/bootstrap4vue2/src/App.vue
@@ -1,46 +1,34 @@
 <template>
   <div id="app">
-    
-    <b-navbar toggleable="md" type="dark" variant="info">
 
-      <b-container>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <!-- Navbar -->
+    <b-container>
+      <b-navbar toggleable="md" type="dark" variant="info">
+
+        <b-container>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-navbar-brand href="#">NavBar</b-navbar-brand>
+          
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item :to="{name: 'home'}" active-class="active" exact>Home</b-nav-item>
+              <b-nav-item :to="{name: 'about'}" active-class="active" exact>About</b-nav-item>
+              <b-nav-item :to="{name: 'grid'}" active-class="active" exact>Grid</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
         
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item :to="{name: 'home'}" active-class="active" exact>Home</b-nav-item>
-            <b-nav-item :to="{name: 'about'}" active-class="active" exact>About</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
+        </b-container>    
       
-      </b-container>    
-    
-    </b-navbar>
+      </b-navbar>
+    </b-container>
+    <!-- Navbar -->
 
-    <router-view/>
+    <!-- Grid, Columnas responsivas [Row/Col] -->
+    <b-container>
+      <router-view/>
+    </b-container>
+    <!-- Grid, Columnas responsivas [Row/Col] -->
+
+      <!-- <router-view/> -->
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ Vue_CLI/bootstrap4vue2/src/views/Grid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-3">
+        <b-row>
+            <b-col cols="12" md="6" lg="4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida dui eu justo vehicula, non convallis sapien sollicitudin. Aenean auctor convallis velit, vel accumsan dolor cursus tempus. Integer fermentum, arcu non pellentesque iaculis, lectus nibh egestas dui, ac efficitur odio quam a metus. Vestibulum in urna rhoncus, mollis risus eu, pharetra sem. Pellentesque eleifend ligula sapien, a lacinia elit sagittis a. Etiam bibendum eu ante id eleifend. Etiam eu turpis sed ex pellentesque finibus at tempus elit. Cras sit amet feugiat risus. Cras eget neque pretium, accumsan ligula nec, bibendum ante. Nunc scelerisque metus sapien, id placerat ex faucibus sed. Maecenas porttitor condimentum tortor, sed luctus leo euismod id. Mauris nisi nisl, rhoncus at lacus vitae, placerat sagittis mi. Etiam vitae vestibulum arcu.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida dui eu justo vehicula, non convallis sapien sollicitudin. Aenean auctor convallis velit, vel accumsan dolor cursus tempus. Integer fermentum, arcu non pellentesque iaculis, lectus nibh egestas dui, ac efficitur odio quam a metus. Vestibulum in urna rhoncus, mollis risus eu, pharetra sem. Pellentesque eleifend ligula sapien, a lacinia elit sagittis a. Etiam bibendum eu ante id eleifend. Etiam eu turpis sed ex pellentesque finibus at tempus elit. Cras sit amet feugiat risus. Cras eget neque pretium, accumsan ligula nec, bibendum ante. Nunc scelerisque metus sapien, id placerat ex faucibus sed. Maecenas porttitor condimentum tortor, sed luctus leo euismod id. Mauris nisi nisl, rhoncus at lacus vitae, placerat sagittis mi. Etiam vitae vestibulum arcu.</p>
+            </b-col>
+        </b-row>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Grid'
+    }
+</script>
