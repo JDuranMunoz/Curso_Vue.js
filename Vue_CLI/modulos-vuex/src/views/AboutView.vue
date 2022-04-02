c592909cc4fe023b8a498f6d953c2908e143665a
JDuranMunoz <javiereduardodm7@gmail.com>
2022-04-01 19:21:12 -0600

    Termine la carpeta 'modulos-vuex'

---
+++ Vue_CLI/modulos-vuex/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2 v-for="(item, index) in tareasState" :key="index">{{item}}</h2>
+    <h3>{{titulo}}</h3>
+    <button @click="aumentarContador(1)">+</button>
+    <h3>{{contador}}</h3>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapMutations } from 'vuex';
+  
+  export default {
+    computed: {
+      ...mapState('tareas', ['tareasState']),
+      ...mapState(['titulo']),
+      ...mapState('contador', ['contador'])
+    },
+    methods: {
+      ...mapMutations('contador', ['aumentarContador'])
+    }
+  }
+</script>
